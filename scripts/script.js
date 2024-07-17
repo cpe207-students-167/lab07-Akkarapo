66d7d60504bcffa29e532e785d0432ea52c7e800
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-07-17 12:49:46 +0700

    Lab7 test2

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,68 @@
-// create reference for input fields.
 const firstNameInput = document.querySelector("#first-name-input");
 const lastNameInput = document.querySelector("#last-name-input");
 const emailInput = document.querySelector("#email-input");
+const submitBtn = document.querySelector("#submit-btn");
 const passwordInput = document.querySelector("#password-input");
 const confirm_passwordInput = document.querySelector("#password-confirm-input");
 const reset_btn = document.querySelector("#reset-btn");
 
-// create reference for buttons.
-const submitBtn = document.querySelector("#submit-btn");
 
-// simple email validation
 function validateEmail(email) {
   var atPos = email.indexOf("@");
   var dotPos = email.lastIndexOf(".");
   return atPos > 0 && dotPos > atPos + 1 && dotPos < email.length - 1;
 }
 
-// add callback function for firstNameInput.onkeyup event
+// add callback functions for other input events.
+// (lastname, email, password, confirm password)
+// validate first name
 firstNameInput.onkeyup = () => {
   firstNameInput.classList.remove("is-valid");
   firstNameInput.classList.remove("is-invalid");
 };
+
+// validate last name
+lastNameInput.onkeyup = () => {
+  lastNameInput.classList.remove("is-valid");
+  lastNameInput.classList.remove("is-invalid");
+};
+
+// validate email
+emailInput.onkeyup = () => {
+  emailInput.classList.remove("is-valid");
+  emailInput.classList.remove("is-invalid");
+};
+
+// validate password
+passwordInput.onkeyup = () => {
+  passwordInput.classList.remove("is-valid");
+  passwordInput.classList.remove("is-invalid");
+};
+
+// validate confirm password
+confirm_passwordInput.onkeyup = () => {
+  confirm_passwordInput.classList.remove("is-valid");
+  confirm_passwordInput.classList.remove("is-invalid");
+};
+
+// add callback function for Reset button.
+reset_btn.onclick = () =>{
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  passwordInput.value = "";
+  confirm_passwordInput.value = "";
+  emailInput.value = "";
+  firstNameInput.classList.remove("is-valid");
+  firstNameInput.classList.remove("is-invalid");
+  lastNameInput.classList.remove("is-valid");
+  lastNameInput.classList.remove("is-invalid");
+  emailInput.classList.remove("is-valid");
+  emailInput.classList.remove("is-invalid");
+  passwordInput.classList.remove("is-valid");
+  passwordInput.classList.remove("is-invalid");
+  confirm_passwordInput.classList.remove("is-valid");
+  confirm_passwordInput.classList.remove("is-invalid");
+}
 
 // add callback functions for other input events.
 // (lastname, email, password, confirm password)
@@ -70,56 +112,3 @@ submitBtn.onclick = () => {
     alert("Registered successfully");
   } 
 };
-
-
-  // validate first name
-  if (firstNameInput.value !== "CPE207") {
-    firstNameInput.classList.add("is-invalid");
-  } else {
-    firstNameInput.classList.add("is-valid");
-    isFirstNameOk = true;
-  }
-
-  // validate last name
-  lastNameInput.onkeyup = () => {
-    lastNameInput.classList.remove("is-valid");
-    lastNameInput.classList.remove("is-invalid");
-  };
-
-  // validate email
-  emailInput.onkeyup = () => {
-    emailInput.classList.remove("is-valid");
-    emailInput.classList.remove("is-invalid");
-  };
-
-  // validate password
-  passwordInput.onkeyup = () => {
-    passwordInput.classList.remove("is-valid");
-    passwordInput.classList.remove("is-invalid");
-  };
-
-  // validate confirm password
-  confirm_passwordInput.onkeyup = () => {
-    confirm_passwordInput.classList.remove("is-valid");
-    confirm_passwordInput.classList.remove("is-invalid");
-  };
-
-  reset_btn.onclick = () =>{
-    firstNameInput.value = "";
-    lastNameInput.value = "";
-    passwordInput.value = "";
-    confirm_passwordInput.value = "";
-    emailInput.value = "";
-    firstNameInput.classList.remove("is-valid");
-    firstNameInput.classList.remove("is-invalid");
-    lastNameInput.classList.remove("is-valid");
-    lastNameInput.classList.remove("is-invalid");
-    emailInput.classList.remove("is-valid");
-    emailInput.classList.remove("is-invalid");
-    passwordInput.classList.remove("is-valid");
-    passwordInput.classList.remove("is-invalid");
-    confirm_passwordInput.classList.remove("is-valid");
-    confirm_passwordInput.classList.remove("is-invalid");
-  }
-
-// add callback function for Reset button.
